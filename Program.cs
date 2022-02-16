dc4c045598ba073975c885e332c7018db037ef6a
nathanparry <nathanparry@trentu.ca>
2022-02-15 20:49:25 -0500

    Fastest route implementation

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
-﻿using static System.Console;
+﻿using System;
 class Program
 {
     /*
         Names:
         Nathan Parry - 0638178
+        Lucas D'Silva -
+        Dylan Haggard - 
     */
 
-    // Main start here 
+    // Main starts here 
     static void Main(string[] args)
     {
         // Airport Nodes
@@ -42,9 +44,44 @@ class Program
         {
             rm.AddAirport(a);
         }
+
+        // try to add another YYZ
+        rm.AddAirport(YYZ);
+
+        // creates the graph/routes
+        rm.AddRoute(YYT, YQM);
+        rm.AddRoute(YYT, YOW);
+        rm.AddRoute(YYT, YUL);
+        rm.AddRoute(YYT, YQB);
+        rm.AddRoute(YYT, YQM);
+        rm.AddRoute(YYT, YQX);
+        rm.AddRoute(YHZ, YYT);
+        // rm.AddRoute(YHZ, YVR); isolates YVR from the graph
+        rm.AddRoute(YHZ, YWG);
+        rm.AddRoute(YFC, YQM);
+        rm.AddRoute(YFC, YYT);
+        rm.AddRoute(YQM, YYZ);
+        rm.AddRoute(YEG, YQM);
+        rm.AddRoute(YYC, YEG);
+        rm.AddRoute(YYZ, YOW);
+        rm.AddRoute(YOW, YEG);
+        rm.AddRoute(YOW, YUL);
+        rm.AddRoute(YQX, YYZ);
+        rm.AddRoute(YQX, YQB);
+        // end of graph
+
+        // displays route map
+        Console.WriteLine(rm);
+
+
+        // Test cases
+        // FastestRoute(YVR, YYZ); // test route for isolated node
+        // FastestRoute(YYZ, YYT); // no pathing to destination connected though
+        // FastestRoute(YQX, YYZ); // DIRECT FLIGHT
+        // FastestRoute(YOW, YOW); // origin = destination
     }
 
-    // SHOULD BE FINISHED NEEDS IMEDIATE TESTING
+    // Fastest route possibly using breadth first search
     static void FastestRoute(AirportNode origin, AirportNode Destination)
     {
         // Modified this for finding fastestroute I think
@@ -54,66 +91,74 @@ class Program
         List<AirportNode> visited = new List<AirportNode>();
         visited.Add(origin);
         AirportNode current = origin;
-        int cnt = 1;
-        while (Q.TryPeek(out current) && !current.Destinations.Contains(Destination))
+        int cnt = 1; // path counter
+        // checks if queue is empty by peeking, if origin contains destination in its list, if origin=destination
+        while (Q.TryPeek(out current) && !current.Destinations.Contains(Destination) && !current.Equals(Destination))
         {
-            current = Q.Dequeue();
-            foreach (AirportNode n in current.Destinations)
+            current = Q.Dequeue(); // dequeues node
+            foreach (AirportNode n in current.Destinations) // checks the current nodes destinations list
             {
-                if (!visited.Contains(n) && !n.Destinations.Contains(Destination)) // destination not found add airport nodes to queue
+                if (!visited.Contains(n) && !n.Destinations.Contains(Destination)) // adds node to queue if not in visited list or in nodes destinations list
                 {
-                    Q.Enqueue(n);
+                    Q.Enqueue(n);   // queues node for next check
                 }
-                else if (n.Destinations.Contains(Destination)) // destination is found
+                else if (n.Destinations.Contains(Destination)) // Destination is in the node's destinations list
                 {
-                    cnt++;
-                    // add relevant airport nodes to visited for route to take
-                    if(!visited.Contains(current)){
-                        visited.Add(current);
+                    cnt++; // increment path counter
+                    // add relevant airport nodes to visited for route to Destination
+                    if (!visited.Contains(current)) // makes sure it's not already in the list
+                    {
+                        visited.Add(current);   // adds the node to the visited list for the route
                     }
-                    visited.Add(n);
-                    visited.Add(Destination);
+                    visited.Add(n); // adds second last node to visited list for the route
+                    visited.Add(Destination);   // adds final destination
 
-                    //outputs for the route
-                    WriteLine("\nPath of {0} From {1} To {2}\nRoute:", cnt, origin.Code, Destination.Code);
+                    //outputs the route
+                    Console.WriteLine("\nPath of {0} From {1} To {2}\nRoute:", cnt, origin.Code, Destination.Code); // the origin and destination are outputted
+
+                    // output the list of visited nodes which is the route to be taken
                     foreach (AirportNode a in visited)
                     {
                         if (a == origin)
                         {
-                            WriteLine("Leaving... {0}", origin.Name);
+                            Console.WriteLine("Leaving... {0}", origin.Name);   // origin
                         }
                         else if (a == Destination)
                         {
-                            WriteLine("Arriving... {0}", Destination.Name);
+                            Console.WriteLine("Arriving... {0}", Destination.Name); // Destination
                         }
                         else
                         {
-                            WriteLine("To {0}", a.Name);
+                            Console.WriteLine("=> {0}", a.Name);    // connecting flights
                         }
                     }
-                    return;
+                    return; // found Destination
                 }
             }
-            if (!visited.Contains(current)) // as long as visited does not contain current airport node add it to visited
+            if (!visited.Contains(current)) // as long as visited does not contain current node add it to visited
             {
                 visited.Add(current);
             }
-            cnt++;
+            cnt++; // increment path counter
         }
         //end of BFS
 
-        // Base Cases of direct flight or no route
-        if (visited.Contains(Destination) || origin.Destinations.Contains(Destination))
+        // Base Cases of direct flight or no route and orgin=Destination
+        if ((visited.Contains(Destination) || origin.Destinations.Contains(Destination)) && !origin.Equals(Destination))
         {
             // fastest case direct flight
-            WriteLine("\nPath of {0} From {1} To {2} \nRoute:", cnt, origin.Code, Destination.Code);
-            WriteLine("Leaving... {0}", origin.Name);
-            WriteLine("Arriving... {0}", Destination.Name);
+            Console.WriteLine("\nPath of {0} From {1} To {2} \nRoute:", cnt, origin.Code, Destination.Code);
+            Console.WriteLine("Leaving... {0}", origin.Name);
+            Console.WriteLine("Arriving... {0}", Destination.Name);
         }
-        else 
+        else if (origin.Equals(Destination))    // origin = Destination
         {
-            // no route
-            WriteLine("\nNo possible route! From {0} To {1}", origin.Name, Destination.Name);
+            Console.WriteLine("\nCurrently at Destination! You are here: \n{0}\n", origin.Name); // already at  destination
+        }
+        else    // no routing
+        {
+            Console.WriteLine("\nNo possible route! From {0} => {1}\n", origin.Name, Destination.Name); // no route
+
         }
     }
 
